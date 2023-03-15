7034fe79951475691ca379388ebe10765737628c
Dominick Agnello <dagnello@asu.edu>
2023-03-14 19:35:50 -0700

    add load sprites feature to constructor and remove function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod rip8;
+use rip8::rip8::Rip8;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut rip = Rip8::new();
+    rip.load_program(args[1].to_string()).unwrap();
+
 }
---
+++ src/rip8/rip8.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+pub struct Rip8 {
+    pub display: Vec<bool>,
+    pub buffer: Vec<u8>,
+    pub stack: Vec<u16>,
+    pub i: u16,
+    pub delay: u8,
+    pub sound: u8,
+    pub pc: u16,
+    pub pause: bool,
+    pub speed: u32,
+}
+
+impl Rip8 {
+    pub fn new() -> Self {
+        let mut buffer = vec![0; 4096];
+
+        let base_sprites = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ];
+
+        for i in 0..base_sprites.len() {
+            buffer[0x0 + i] = base_sprites[i];
+        }
+
+        Self {
+            display: vec![false; 64 * 32],
+            buffer,
+            stack: vec![0; 16],
+            i: 0x200,
+            delay: 0,
+            sound: 0,
+            pc: 0x200,
+            pause: false,
+            speed: 10,
+        }
+    }
+
+    pub fn load_program(&mut self, path: String) -> io::Result<()> {
+        // some code to ensure proper bytes
+
+        let mut f = File::open(path)?;
+        let mut buffer: Vec<u8> = Vec::new();
+        // perhaps can read directly into the buffer to save time?
+        match  f.read_to_end(&mut buffer) {
+            Ok(_) => {
+                for i in 0..buffer.len() {
+                    self.buffer[0x200 + i] = buffer[i];
+                }
+                println!("loaded the program!");
+                Ok(())
+            }
+            Err(e) => {
+                Err(e)
+            }
+        }
+    }
+
+    // for now
+    #[allow(unused)]
+    pub fn set_pixel(&mut self, x: i8, y: i8) {
+        //swap pixel values
+
+        // handles index wrapping
+        let mut x_wrap: usize = 0;
+        let mut y_wrap: usize = 0;
+        if x >= 64 {
+            x_wrap = (x % 64).try_into().unwrap();
+        } else if x < 0 {
+            x_wrap = (64 - (x.abs() % 64)).try_into().unwrap();
+        }
+        if y >= 32 {
+            y_wrap = (y % 32).try_into().unwrap();
+        } else if y < 0 {
+            y_wrap = (32 - (x.abs() % 32)).try_into().unwrap();
+        }
+        self.display[y_wrap * 64 + x_wrap] = !self.display[y_wrap * 64 + x_wrap]
+    }
+}
